76180ef5601ba6fff9ebf699515db114a5ef2b8c
Muhamad Ridwan <ridwanwan784@gmail.com>
2022-07-07 15:04:36 +0700

    menambahkan script js

---
+++ js/script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const submitForm = document.getElementById('form');
+    submitForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        addTodo()
+    })
+})
+
+function addTodo() {
+    const textTodo = document.getElementById('title').value;
+    const txtDate = document.getElementById('date').value;
+
+    const generatedID = generateId();
+    const TodoObject = generateTodoObject(generatedID, textTodo, timestamp, false);
+
+    todos.push(TodoObject);
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function generateId() {
+    return +new Date();
+}
+
+function generateTodoObject(id, task, timestamp, isCompleted) {
+    return {
+        id, 
+        task, 
+        timestamp, 
+        isCompleted
+    }
+}
+
+const todos = [];
+const RENDER_EVENT = 'render-todo';
+
+document.addEventListener(RENDER_EVENT, function () {
+    console.log(todos);
+  });
